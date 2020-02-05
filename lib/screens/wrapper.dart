fbf37383de150c17f4d02e7be312e7c8e17fda98
undefinedzack <undefinedzacky@gmail.com>
2020-02-05 19:23:59 +0530

    Authentication

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:kotgirwar_fan_club/screens/authenticate/authenticate.dart';
+//import 'package:kotgirwar_fan_club/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    //return home or authenticate widget
+    
+    return Authenticate();
+  }
+}
